4c5031611f1812861c6ca4272755e31a3de3cbc7
AJITH KUMAR A <12ajith1998@gmail.com>
2020-03-11 16:52:08 +0530

    Update ArrayIndexOutOfBound_Demo.java

--- Exceptions/ArrayIndexOutOfBound_Demo.java
+++ Exceptions/ArrayIndexOutOfBound_Demo.java
@@ -6,7 +6,8 @@ class ArrayIndexOutOfBound_Demo
 			int a[] = new int[5]; 
 			a[6] = 9; 
 		} 
-		catch(ArrayIndexOutOfBoundsException e){ 
+		catch(ArrayIndexOutOfBoundsException e)
+		{ 
 			System.out.println ("Array Index is Out Of Bounds"); 
 		} 
 	} 
