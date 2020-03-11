92aef7a3d156221591aa8441e2227d972e63f60b
AJITH KUMAR A <12ajith1998@gmail.com>
2020-03-11 13:40:51 +0530

    Add files via upload

---
+++ Arrays/Multi_Dim.java
@@ -0,0 +1,16 @@
+class Multi_Dim
+{  
+public static void main(String args[])
+{  
+
+	int arr[][]={{9,8,7},{6,5,4},{3,2,1}};  
+    for(int i=0;i<3;i++)
+    {  
+    for(int j=0;j<3;j++)
+    {  
+    System.out.print(arr[i][j]+" ");  
+    }  
+    System.out.println();  
+    }  
+}
+} 
