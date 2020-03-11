bce848cbe6315858c3eba120d6a4d521c33a3ab8
AJITH KUMAR A <12ajith1998@gmail.com>
2020-03-11 13:41:47 +0530

    Add files via upload

---
+++ Enum1.java
@@ -0,0 +1,13 @@
+public class Enum1 
+{ 
+	enum Color 
+	{ 
+		RED, GREEN, BLUE; 
+	} 
+
+	public static void main(String[] args) 
+	{ 
+		Color c1 = Color.RED; 
+		System.out.println(c1); 
+	} 
+}
---
+++ StringExample.java
@@ -0,0 +1,13 @@
+public class StringExample
+{  
+public static void main(String args[])
+{  
+String s1="java";
+char ch[]={'A','J','I','T','H'};  
+String s2=new String(ch); 
+String s3=new String("kingmakerAjith");  
+System.out.println(s1);  
+System.out.println(s2);  
+System.out.println(s3);  
+}
+}  
