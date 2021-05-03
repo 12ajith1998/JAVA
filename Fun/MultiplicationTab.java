2345ed7abe130cecb4307dc09648de7a7134b626
AJITH KUMAR A <12ajith1998@gmail.com>
2021-05-03 15:38:39 +0530

    Create MultiplicationTab.java

---
+++ Fun/MultiplicationTab.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MultiplicationTab {
+
+	public static void main(String[] args) 
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		
+		System.out.println("Enter the mult tables NUM: ");
+		
+		for (int i = 0; i < 10; i++) 
+		{
+			System.out.println(a+"x"+(i+1)+" = "+(a*(i+1)));
+		}
+		
+	}
+
+}
