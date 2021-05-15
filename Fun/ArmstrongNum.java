56952a6224fd4199c6994af1466342a86a70a06a
AJITH KUMAR A <12ajith1998@gmail.com>
2021-05-15 21:37:19 +0530

    Create ArmstrongNum.java

---
+++ Fun/ArmstrongNum.java
@@ -0,0 +1,27 @@
+
+public class ArmstrongNum {
+
+	public static void main(String[] args) 
+	{
+		int n=153;
+		int temp=n;
+		
+		int r,sum=0;
+		
+		while(n>0)
+		{
+		r=n%10;
+		n=n/10;
+		sum=sum+ r*r*r;
+		}
+		if(temp==sum)
+		{
+			System.out.println("ArmstrongNumber");
+		}
+		else
+		{
+			System.out.println("Not a ArmstrongNumber");
+		}
+		
+	}
+}
