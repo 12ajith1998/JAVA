6aa4aa98bdf77e753abddf67c58fb9b6de222ec5
AJITH KUMAR A <12ajith1998@gmail.com>
2021-06-29 11:52:21 +0530

    Create FindPos.java

---
+++ Arrays/FindPos.java
@@ -0,0 +1,25 @@
+public class FindPos {
+
+	public static int findIndex(int[] arr,int t)
+	{
+		if(arr==null) return -1;
+		int i=0;
+		while(i<arr.length)
+		{
+			if(arr[i]==t)
+			{
+				return i;
+			}else
+			{
+				i=i+1;
+			}
+		}
+		
+		return -1;
+	}
+	public static void main(String[] args) 
+	{
+		int[] arr= {1,2,3,4,5,6,7,8,9,10};
+		System.out.println("index pos is 3:"+findIndex(arr,1));
+	}
+}
